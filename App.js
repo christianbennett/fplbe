0b4c770e09e7445ce1e4b13fce448957ae0b1a5b
Bayu Adjie Sidharta <bayuaaas@gmail.com>
2020-11-06 21:11:10 +0700

    2nd Commit

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Testing Github</Text>
+      <Text>This doesn't seem to work</Text>
       <StatusBar style="auto" />
     </View>
   );
